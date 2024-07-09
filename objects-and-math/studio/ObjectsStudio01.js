a10459aa0f0a06c62899a1de2db2b9a0083a794c
Mohamed <issine14@gmail.com>
2024-07-08 20:20:21 -0500

    studio class 7

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,51 @@
 // Code your selectRandomEntry function here:
-
+function selectRandomEntry(arr){
+   return rondonIdex=arr[Math.floor(Math.random()*arr.length)];
+   
+}
 
 // Code your buildCrewArray function here:
 
+function select3IDfrom(arr){
+   let selectedIds=[];
+
+   while(selectedIds.length<3){
+      let id=selectRandomEntry(arr);
+      if(!selectedIds.includes(id)){
+        selectedIds.push(id);
+      }
+   }
+
+ return selectedIds;
+}
+
+
+
+
+function crewSelection( idsArray, animalsArray){
+    let crew=[];
+     
+    for(let i=0;i<idsArray.length;i++){
+       let id=idsArray[i];
+       for(j=0;j<animalsArray.length;j++){
+          let  animal=animalsArray[j];
+          if(animal["astronautID"]===id){
+             crew.push(animal);
+          }
+
+       }
+
+
+    }
+  return crew;
+};
+
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
+
+console.log(selectRandomEntry(idNumbers));
+console.log(select3IDfrom(idNumbers));
+
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -53,3 +94,9 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+  
+  
+//'____, ____, and ____ are going to space!'
+let selections=select3IDfrom(idNumbers);
+let crew=crewSelection(selections,animals);
+console.log(`${crew[0].name}, ${crew[1].name}, and ${crew[2].name} are going to space!` );
