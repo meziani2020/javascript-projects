cbb2ae89367e5360b8c20101b2fb968b1d6e408e
Mohamed <issine14@gmail.com>
2024-07-18 21:04:59 -0500

    exercises Exceptions

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,25 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+function divide(numerator , denominator){
+ 
+  
+       if(typeof denominator  =='undefined'){
+        throw Error("Attempted to divide by an undefined "); 
+        }
+
+        if(denominator==0){
+             throw Error("Attempted to divide by zero.");
+             
+         }
+
+         return  numerator / denominator ;
+
+        }
+
+
+    
+
+
+
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,12 +1,30 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
+
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try {
+    result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+    }catch(err){
+      
+      if(typeof lab.runLab =="undefined"){
+        console.log('looks like the "runLab" property is not defined. ');
+        result ="Error thrown";
+      }
+
+    }
+    
   }
 }
 
 let studentLabs = [
+  {
+    student: 'Blake',
+    myCode: function (num) {
+      return Math.pow(num, num);
+    }
+},
   {
     student: 'Carly',
     runLab: function (num) {
