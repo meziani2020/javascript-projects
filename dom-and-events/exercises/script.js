44e60858392706d6f430037b5a34fdc410262821
Mohamed <issine14@gmail.com>
2024-08-05 19:10:46 -0500

    dom event exericese

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,35 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+
+
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+
+     missionAbort.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "red";
+     });
+     
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });  
+
+     missionAbort.addEventListener("click", function(ev){
+        let ok=confirm("Are you sure you want to abort the mission?" );
+        if(ok){
+            paragraph.innerHTML = ' Mission aborted! Space shuttle returning home';
+        }
+
+
+
+     })
+     
+     
+
 }
 
 window.addEventListener("load", init);
+
+
